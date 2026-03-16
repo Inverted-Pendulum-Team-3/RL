b754464fb8ab4b70014a3c1cd97c31bf57087fc5
dylanleroy2603 <dylan_leroy@tamu.edu>
2026-03-16 09:23:57 -0500

    pyb_env.py
    current best 3/16

--- pyb_env.py
+++ pyb_env.py
@@ -15,23 +15,24 @@ def create_env():
     planeId = p.loadURDF("plane.urdf")
     
     #Load robot from URDF, arbitrary start position, and standing starting orientation
-    startPos = [0, 0, .15]
+    startPos = [0, 0, .17]
     startOrientation = p.getQuaternionFromEuler([0, 0, 0.7])
-    boxId = p.loadURDF("C:\\Users\\dylan\\Documents\\Semester_9\\CAPSTONE\\Robot_RL\\Balro3.urdf", startPos, startOrientation)
-    
+    boxId = p.loadURDF("C:\\Users\\dylan\\Desktop\\WorkingRL\\balro4.urdf", startPos, startOrientation)
     #Set friction and gravity
-    p.changeDynamics(planeId, -1, lateralFriction=10.0)
-    p.changeDynamics(boxId, 1, lateralFriction=3.0)
-    p.changeDynamics(boxId, 0, lateralFriction=3.0)
+    p.changeDynamics(planeId, -1, lateralFriction=1.0)
+    p.changeDynamics(boxId, 1, lateralFriction=1.0, jointDamping=0.3)
+    p.changeDynamics(boxId, 0, lateralFriction=1.0, jointDamping=0.3)
     p.setGravity(0,0,-9.8) 
     
     return boxId
     
-def step_sim(velocity1, velocity2, boxId, target_velocity, target_rot_rate):
+def step_sim(velocity1, velocity2, boxId, target_velocity, target_rot_rate, total_timesteps):
     #Set motor speeds and step simulation
     p.setJointMotorControl2(bodyUniqueId = boxId, jointIndex=0, controlMode=p.VELOCITY_CONTROL, targetVelocity = velocity1 * 12.775, force= MAXFORCE)
     p.setJointMotorControl2(bodyUniqueId = boxId, jointIndex=1, controlMode=p.VELOCITY_CONTROL, targetVelocity = velocity2 * 12.775, force= MAXFORCE)
-    p.stepSimulation()
+    
+    for i in range(4):
+        p.stepSimulation()
     
     #get velocity of robot (velocity of forward movement)
     linear_velocity = get_linear_velocity(boxId)
@@ -46,33 +47,13 @@ def step_sim(velocity1, velocity2, boxId, target_velocity, target_rot_rate):
     wheel_velocity_1 = p.getJointState(boxId, 0)[1]
     wheel_velocity_2 = p.getJointState(boxId, 1)[1]
 
+    velocity_error = target_velocity - linear_velocity
+    rot_rate_error = target_rot_rate - rot_rate
     
-    #Display pendulum angle and sleep
-    #p.addUserDebugText("Pendulum angle: {:.2f}".format(roll * 57), [0, 0, 1.2], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.041)
-    #p.addUserDebugText("Target Velocity: {:.2f}".format(target_velocity), [0, 0, 1], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-    #p.addUserDebugText("Linear Velocity: {:.2f}".format(linear_velocity), [0, 0, 1.4], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-    #p.addUserDebugText("Velocity Compliance: {:.2f}".format(-1 * abs(target_velocity - (linear_velocity * 0.2))), [0, 0, 1.6], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-    #p.addUserDebugText("Linear Velocity: {:.2f}".format(linear_velocity), [0, 0, 1], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-    #p.addUserDebugText("Target Velocity: {:.2f}".format(target_velocity), [0, 0, 1.2], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-    #p.addUserDebugText("Roll: {:.2f}".format(roll), [0, 0, 1.2], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-    #p.addUserDebugText("Pitch: {:.2f}".format(pitch * 10), [0, 0, 1.4], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-    #p.addUserDebugText("Roll Rate: {:.2f}".format(roll_rate * 0.3), [0, 0, 1.6], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-    #p.addUserDebugText("Rot. Rate: {:.2f}".format(rot_rate), [0, 0, 1.8], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-    #p.addUserDebugText("Wheel Velocity 1: {:.2f}".format(velocity1), [0, 0, 2], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-    #p.addUserDebugText("Wheel Velocity 2: {:.2f}".format(velocity2), [0, 0, 2.2], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-    #p.addUserDebugText("Target Velocity: {:.2f}".format(target_velocity), [0, 0, 2.4], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-    #p.addUserDebugText("Target Velocity: {:.2f}".format(target_rot_rate), [0, 0, 2.6], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
+    experience_scale = min(1.0, total_timesteps / 200000)
     
-    #log_data("state_vals/linear_velocity.csv", linear_velocity*0.8)
-    #log_data("state_vals/roll.csv", roll * 3.33)
-    #log_data("state_vals/pitch.csv", pitch * 5)
-    #log_data("state_vals/roll_rate.csv", roll_rate * 0.5)
-    #log_data("state_vals/rot_rate.csv", rot_rate*0.1333)
-    #log_data("state_vals/wheel_velocity_1.csv", wheel_velocity_1)
-    #log_data("state_vals/wheel_velocity_2.csv", wheel_velocity_2)
-  
     
-    state = linear_velocity, roll, pitch, roll_rate, rot_rate, wheel_velocity_1, wheel_velocity_2, target_velocity, target_rot_rate
+    state = linear_velocity + np.random.normal(0,0.05 * experience_scale), roll + np.random.normal(0,0.005 * experience_scale), roll_rate+ np.random.normal(0,0.01 * experience_scale), rot_rate + np.random.normal(0,0.01 * experience_scale), wheel_velocity_1 + np.random.normal(0,0.05 * experience_scale), wheel_velocity_2 + np.random.normal(0,0.05 * experience_scale), velocity_error, rot_rate_error
     return state
 
 def get_linear_velocity(boxId):
@@ -120,12 +101,8 @@ def get_pitch_rate(robot_id):
 
     #Extract pitch rate and roll rate
     roll_rate = float(ang_vel_body[0])
-    pitch_rate = float(ang_vel_body[1])
     rot_rate  = float(ang_vel_body[2])
-    #p.addUserDebugText("roll rate: {:.2f}".format(roll_rate), [0, 0, 1.4], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-    #p.addUserDebugText("pitch rate: {:.2f}".format(pitch_rate), [0, 0, 1.6], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-    #p.addUserDebugText("rot rate: {:.2f}".format(rot_rate), [0, 0, 1.8], textColorRGB=[1,0,0], textSize=1.5, lifeTime=0.1)
-
+    
     return roll_rate, rot_rate
 
     
@@ -137,4 +114,3 @@ def log_data(file, data):
             
 def env_disconnect():
     p.disconnect()
-    
